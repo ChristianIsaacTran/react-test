65ce83c307a8c3e17e5a5341b4689f24a5362c70
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-11-12 20:05:59 -0600

    feat: added a greeting component and pictureFrame component
    Practicing making components and making JSX
    return statements. React components are just javascript functions
    that return JSX which is HTML. After making it,
    we use ES6 importing/exporting to import it into the main.jsx
    and call the components. Note: all components must be
    capitalized to differentiate between a react component
    and a regular HTML statement. Importing and exporting
    default and named works the same way in these cases.

---
+++ src/Greeting.jsx
@@ -0,0 +1,3 @@
+export default function MyNamesRobert() {
+    return <h1>&quot;Nevermind, look sharp. We're savin' reality.&quot; - John Jones Fortnite</h1>;
+}
---
+++ src/pictureFrame.jsx
@@ -0,0 +1,13 @@
+
+
+
+function PictureFrame() {
+    return <img src="../public/jonesyWisdom.png" alt="jonesy smoking a fat one with tomatonator" />;
+}
+
+
+function PictureText() {
+    return <h3>Jonesy in fortnite </h3>
+}
+
+export {PictureFrame, PictureText};
